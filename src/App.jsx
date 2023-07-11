8eb7f9f6de24789998baed4bb8a45adb527b6403
kevin <kevin@puhanda.com>
2023-07-11 14:04:40 +0800

    feat: refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState, useMemo } from 'react'
-import TodoCount from './TodoCount'
+import { useState } from 'react'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import TodoFilter from './TodoFilter'
 import { v4 as uuid } from 'uuid'
 
+const applyFilter = (todos, filter) => {
+  switch(filter) {
+    case 'all':
+      return todos
+    case 'completed':
+      return todos.filter(todo => todo.completed)
+    case 'uncompleted':
+      return todos.filter(todo => !todo.completed)
+    default:
+      return todos
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
   const [todos, setTodos] = useState([])
   const [filter, setFilter] = useState('all')
 
@@ -20,31 +30,9 @@ function App() {
       },
       ...preTodos
     ])
-    setCount(todos.length)
   }
 
-  const filterTodos = useMemo(() => {
-    let result;
-    switch(filter) {
-      case 'all':
-        result = todos.filter(todo => todo != null)
-        setCount(result.length)
-        return result
-      case 'completed':
-        result =  todos.filter(todo => todo.completed)
-        setCount(result.length)
-        return result
-      case 'uncompleted':
-        result = todos.filter(todo => !todo.completed)
-        setCount(result.length)
-        return result
-      default:
-        result = todos.filter(todo => todo != null)
-        setCount(result.length)
-        return result
-    }
-    
-  }, [todos, filter])
+  const filterTodos = applyFilter(todos, filter)
 
   const handleDelete = (todo) => {
     setTodos((preTodos) => preTodos.filter(item => item.id !== todo.id))
@@ -64,7 +52,6 @@ function App() {
 
   return (
     <div style={{ maxWidth: '400px', border: '1px solid grey', padding: '20px'}}>
-      <TodoCount count={count} />
       <TodoForm createTodo={createTodo} />
       <TodoList filterTodos={filterTodos} handleComplete={handleComplete} handleDelete={handleDelete} />
       <TodoFilter setFilter={setFilter} />
--- src/TodoCount.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TodoCount = ({ count }) => {
-  return (
-    <div>任务数量：{count}</div>
-  )
-}
-
-export default TodoCount
